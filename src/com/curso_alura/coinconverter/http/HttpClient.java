918d4feab8d7978fee8f8638386339e3e30e77a1
Ricardo <ricardo-jsb@hotmail.com>
2024-06-13 11:05:28 +0200

    feat-httpCliente

--- src/com/curso_alura/coinconverter/http/HttpClient.java
+++ src/com/curso_alura/coinconverter/http/HttpClient.java
@@ -1,1 +1,32 @@
+package com.curso_alura.coinconverter.http;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpClient {
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+
+    public String sendGetRequest(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Sending 'GET' request to URL: " + url);
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+}
